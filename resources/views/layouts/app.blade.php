b3c7cedd5b3de8b1fbf8358ff9bd1b602cbfe1b5
Fabiopanetta02 <panetta.fabio20@gmail.com>
2022-09-15 13:20:05 +0200

    Add app layout and homepage basic

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel DC Comics | @yield('page-title', 'homepage')</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+        </style>
+    </head>
+    <body>
+        <header id="site_header"></header>
+        <!-- /#site_header -->
+
+        <main id="site_main">
+        </main>
+        <!-- /#site_main -->
+
+        <footer></footer>
+        <!-- /#site_footer -->
+
+    </body>
+</html>
