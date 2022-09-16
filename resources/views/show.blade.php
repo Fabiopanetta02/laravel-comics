34dab6578f839fc8d9cc051064ec95aa9d910dd4
Fabiopanetta02 <panetta.fabio20@gmail.com>
2022-09-16 13:59:58 +0200

    Added completd section description single album

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -17,8 +17,27 @@
     </div>
 </div>
 
-<div class="container">
-
-</div>
-
+<div class="single_album container">
+    <div class="row d_flex">
+        <div class="col-75">
+             <h2> {{ strtoupper($album['title']) }} </h2>
+             <div class="single_album_info">
+                 <div class="avaiable col-75">
+                     <p>U.S. Price: <span>{{ $album['price'] }}</span></p>
+                     <p>AVAIABLE</p>
+                 </div>
+                 <div class="check col-25 t_center">
+                     <p>Check Availability</p>
+                 </div>
+             </div>
+             <div class="description">
+                 <p>{{ $album['description'] }} </p>
+             </div>
+         </div>
+         <div class="col-25 rewards">
+             <p>ADVERTISEMENT</p>
+             <img src="{{asset('images/adv.jpg')}}" alt="Superman">
+         </div>
+     </div>
+ </div>
 @endsection
